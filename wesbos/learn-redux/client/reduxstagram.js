2bbf7498cb4cecfc6f114ecec6565e3cdba9d827
azizultex <azizultex@gmail.com>
2017-10-26 13:25:59 +0600

    complete routing on learn-redux

--- wesbos/learn-redux/client/reduxstagram.js
+++ wesbos/learn-redux/client/reduxstagram.js
@@ -1,1 +1,25 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+// import css 
+import css from './styles/style.styl';
+
+// import components 
+import Main from './components/Main';
+import PhotoGrid from './components/PhotoGrid';
+import Single from './components/Single';
+
+// import react router
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+// render elements 
+const route = (
+		<Router history={browserHistory}>
+			<Route path="/" component={Main}>
+				<IndexRoute component={PhotoGrid}></IndexRoute>
+				<Route path="/view/:postId" component={Single}></Route>
+			</Route>
+		</Router>
+	)
+
+render(route, document.getElementById('root'));
